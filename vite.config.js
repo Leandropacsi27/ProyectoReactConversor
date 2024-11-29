7f09b8418dafd579661b2631f4654d40c6e93009
Leandropacsi27 <137239458+Leandropacsi27@users.noreply.github.com>
2024-11-28 20:52:50 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/ProyectoReactConversor/",
+  plugins: [react()],
+  base: "https://Leandropacsi27.github.io/ProyectoReactConversor"
 });
 
